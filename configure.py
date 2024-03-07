5bd34906b2f8f0ad4ca40eacc7b5ba79f2ab1da8
Robin Avery <robin@ribbanya.dev>
2024-03-07 01:26:41 -0500

    wip

--- configure.py
+++ configure.py
@@ -5,47 +5,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List, Set, Tuple
 
-
-@dataclass
-class Archive:
-    src: Path
-    dst: Path
-    manifest: Set[Path]
-
-
-@dataclass
-class Object:
-    src: Path
-    base: Path
-    target: Path
-    dwarf: Path
-    mwcc: Path
-    cflags: List[str]
-
-
-# TODO name
-@dataclass
-class Unit:
-    complete: bool
-    name: str
-    signed_char: bool = False
-
-
-@dataclass
-class Profile:
-    name: str
-    archive_suffix: str
-    cflags: List[str]
-
-
-@dataclass
-class Diff:
-    name: str
-    target_path: Path
-    base_path: Path
-    reverse_fn_order: bool
-    complete: bool
-
+from tools.project import BuildConfig, Lib, Profile, ProjectConfig, Unit
 
 CFLAGS = [
     "-nodefaults",
@@ -87,106 +47,34 @@ PROFILES = [
 ]
 
 
-@dataclass
-class ProjectConfig:
-    version: str
-    src_dir = Path("src")
-    build_dir = Path("build")
-    orig_dir = Path("orig")
-    mwcc = Path("GC/1.2.5")
+config = ProjectConfig(PROFILES)
 
-    @property
-    def output_dir(self) -> Path:
-        return self.build_dir / self.version
-
-    @property
-    def archive_dir(self) -> Path:
-        return self.orig_dir / self.version
+build = BuildConfig()
 
 
-config = ProjectConfig("DOLSDK-2001-05-22")
+def lib(name: str, *units: Unit) -> Lib:
+    return Lib(config, name, units)
 
 
-@dataclass
-class Lib:
-    archives: List[Archive]
-    objects: List[Object]
-    diffs: List[Diff]
-
-    def __init__(self, name: str, *units: Unit) -> None:
-        self.archives = []
-        self.objects = []
-        self.diffs = []
-
-        for profile in PROFILES:
-
-            def make_dir(dir_name: str) -> Path:
-                return config.output_dir / dir_name / profile.name / name
-
-            archive_name = f"{name}{profile.archive_suffix}.a"
-            target_dir = make_dir("src")
-            base_dir = make_dir("obj")
-            dwarf_dir = make_dir("dwarf")
-
-            def create_pair(u: Unit) -> Tuple[Object, Diff]:
-                src_name = Path(u.name)
-                obj_name = src_name.with_suffix(".o")
-                base_path = base_dir / obj_name
-                target_path = target_dir / obj_name
-                obj = Object(
-                    src=config.src_dir / name / src_name,
-                    base=base_path,
-                    target=target_path,
-                    dwarf=dwarf_dir / src_name.with_suffix(".c"),
-                    mwcc=Path("GC/1.2.5"),
-                    cflags=[
-                        f"-char {'signed' if u.signed_char else 'unsigned'}",
-                        *profile.cflags,
-                    ],
-                )
-
-                diff = Diff(
-                    name=str(
-                        (Path(profile.name) / name / src_name)
-                        .with_suffix("")
-                        .as_posix()
-                    ),
-                    target_path=target_path,
-                    base_path=base_path,
-                    reverse_fn_order=False,
-                    complete=u.complete,
-                )
-
-                return obj, diff
-
-            pairs = list(map(create_pair, units))
-
-            self.archives.append(
-                Archive(
-                    src=config.archive_dir / archive_name,
-                    dst=base_dir,
-                    manifest=set(o.base.relative_to(base_dir) for o, _ in pairs),
-                )
-            )
-            self.objects.extend(obj for obj, _ in pairs)
-            self.diffs.extend(diff for _, diff in pairs)
+def unit(complete: bool, name: str, signed_char=False):
+    return Unit(name, complete, signed_char)
 
 
 Matching = True
 NonMatching = False
 
 
-libs = [
-    Lib(
+LIBS = [
+    lib(
         "os",
-        Unit(NonMatching, "OSReboot.c", signed_char=True),
-        Unit(Matching, "OSAlloc.c"),
+        unit(NonMatching, "OSReboot.c", signed_char=True),
+        unit(Matching, "OSAlloc.c"),
     )
 ]
 
 
 def main():
-    pprint.pp(libs)
+    pprint.pp(build)
 
 
 if __name__ == "__main__":
---
+++ tools/project.py
@@ -0,0 +1,163 @@
+import io
+import sys
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Iterable, List, Optional, Set, Tuple
+
+import ninja_syntax
+
+if sys.platform == "cygwin":
+    sys.exit(
+        f"Cygwin/MSYS2 is not supported."
+        f"\nPlease use native Windows Python instead."
+        f"\n(Current path: {sys.executable})"
+    )
+
+
+@dataclass
+class Args:
+    pass
+
+
+@dataclass
+class Archive:
+    src: Path
+    dst: Path
+    manifest: Set[Path]
+
+
+@dataclass
+class Object:
+    src: Path
+    base: Path
+    target: Path
+    dwarf: Path
+    mwcc: Path
+    cflags: List[str]
+
+
+# TODO name
+@dataclass
+class Unit:
+    name: str
+    complete: bool
+    signed_char: bool = False
+
+
+@dataclass
+class Profile:
+    name: str
+    archive_suffix: str
+    cflags: List[str]
+
+
+@dataclass
+class Diff:
+    name: str
+    target_path: Path
+    base_path: Path
+    reverse_fn_order: bool
+    complete: bool
+
+
+@dataclass
+class ProjectConfig:
+    profiles: List[Profile]
+    version: Optional[str] = field(default=None)
+    src_dir = Path("src")
+    build_dir = Path("build")
+    orig_dir = Path("orig")
+    mwcc = Path("GC/1.2.5")
+
+    @property
+    def output_dir(self) -> Path:
+        return self.build_dir / self.version
+
+    @property
+    def archive_dir(self) -> Path:
+        return self.orig_dir / self.version
+
+
+@dataclass
+class Lib:
+    archives: List[Archive]
+    objects: List[Object]
+    diffs: List[Diff]
+
+    def __init__(self, config: ProjectConfig, name: str, units: Iterable[Unit]) -> None:
+        self.archives = []
+        self.objects = []
+        self.diffs = []
+
+        for profile in config.profiles:
+
+            def make_dir(dir_name: str) -> Path:
+                return config.output_dir / dir_name / profile.name / name
+
+            archive_name = f"{name}{profile.archive_suffix}.a"
+            target_dir = make_dir("src")
+            base_dir = make_dir("obj")
+            dwarf_dir = make_dir("dwarf")
+
+            def create_pair(u: Unit) -> Tuple[Object, Diff]:
+                src_name = Path(u.name)
+                obj_name = src_name.with_suffix(".o")
+                base_path = base_dir / obj_name
+                target_path = target_dir / obj_name
+                obj = Object(
+                    src=config.src_dir / name / src_name,
+                    base=base_path,
+                    target=target_path,
+                    dwarf=dwarf_dir / src_name.with_suffix(".c"),
+                    mwcc=Path("GC/1.2.5"),
+                    cflags=[
+                        f"-char {'signed' if u.signed_char else 'unsigned'}",
+                        *profile.cflags,
+                    ],
+                )
+
+                diff = Diff(
+                    name=str(
+                        (Path(profile.name) / name / src_name)
+                        .with_suffix("")
+                        .as_posix()
+                    ),
+                    target_path=target_path,
+                    base_path=base_path,
+                    reverse_fn_order=False,
+                    complete=u.complete,
+                )
+
+                return obj, diff
+
+            pairs = list(map(create_pair, units))
+
+            self.archives.append(
+                Archive(
+                    src=config.archive_dir / archive_name,
+                    dst=base_dir,
+                    manifest=set(o.base.relative_to(base_dir) for o, _ in pairs),
+                )
+            )
+            self.objects.extend(obj for obj, _ in pairs)
+            self.diffs.extend(diff for _, diff in pairs)
+
+
+@dataclass
+class Tool:
+    pass
+
+
+@dataclass
+class BuildConfig:
+    archives: List[Archive] = field(default_factory=list)
+    objects: List[Object] = field(default_factory=list)
+    diffs: List[Diff] = field(default_factory=list)
+    tools: List[Tool] = field(default_factory=list)
+
+
+def generate_build() -> None:
+    out = io.StringIO()
+    n = ninja_syntax.Writer(out)
+    n.variable("ninja_required_version", "1.3")
+    n.newline()
