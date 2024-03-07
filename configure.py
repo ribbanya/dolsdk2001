fefb04cde8bf1d7460e662b274e03eb683bea2a5
Robin Avery <robin@ribbanya.dev>
2024-03-07 00:42:55 -0500

    wip

--- configure.py
+++ configure.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-import json
 import pprint
-import sys
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List, Optional, Set
+from typing import List, Set, Tuple
 
 
 @dataclass
@@ -40,7 +38,14 @@ class Profile:
     cflags: List[str]
 
 
-# CHARFLAGS := -char unsigned
+@dataclass
+class Diff:
+    name: str
+    target_path: Path
+    base_path: Path
+    reverse_fn_order: bool
+    complete: bool
+
 
 CFLAGS = [
     "-nodefaults",
@@ -55,7 +60,6 @@ CFLAGS = [
     "-ir src",
 ]
 
-# INCLUDES := -Iinclude -ir src
 
 PROFILES = [
     Profile(
@@ -107,10 +111,13 @@ config = ProjectConfig("DOLSDK-2001-05-22")
 class Lib:
     archives: List[Archive]
     objects: List[Object]
+    diffs: List[Diff]
 
     def __init__(self, name: str, *units: Unit) -> None:
         self.archives = []
         self.objects = []
+        self.diffs = []
+
         for profile in PROFILES:
 
             def make_dir(dir_name: str) -> Path:
@@ -121,13 +128,16 @@ class Lib:
             base_dir = make_dir("obj")
             dwarf_dir = make_dir("dwarf")
 
-            def create_obj(u: Unit):
-                obj_name = Path(u.name)
-                return Object(
-                    src=config.src_dir / name / obj_name,
-                    base=base_dir / obj_name.with_suffix(".o"),
-                    target=target_dir / obj_name.with_suffix(".o"),
-                    dwarf=dwarf_dir / obj_name.with_suffix(".c"),
+            def create_pair(u: Unit) -> Tuple[Object, Diff]:
+                src_name = Path(u.name)
+                obj_name = src_name.with_suffix(".o")
+                base_path = base_dir / obj_name
+                target_path = target_dir / obj_name
+                obj = Object(
+                    src=config.src_dir / name / src_name,
+                    base=base_path,
+                    target=target_path,
+                    dwarf=dwarf_dir / src_name.with_suffix(".c"),
                     mwcc=Path("GC/1.2.5"),
                     cflags=[
                         f"-char {'signed' if u.signed_char else 'unsigned'}",
@@ -135,16 +145,31 @@ class Lib:
                     ],
                 )
 
-            objects = list(map(create_obj, units))
+                diff = Diff(
+                    name=str(
+                        (Path(profile.name) / name / src_name)
+                        .with_suffix("")
+                        .as_posix()
+                    ),
+                    target_path=target_path,
+                    base_path=base_path,
+                    reverse_fn_order=False,
+                    complete=u.complete,
+                )
+
+                return obj, diff
+
+            pairs = list(map(create_pair, units))
 
             self.archives.append(
                 Archive(
                     src=config.archive_dir / archive_name,
                     dst=base_dir,
-                    manifest=set(o.base.relative_to(base_dir) for o in objects),
+                    manifest=set(o.base.relative_to(base_dir) for o, _ in pairs),
                 )
             )
-            self.objects.extend(objects)
+            self.objects.extend(obj for obj, _ in pairs)
+            self.diffs.extend(diff for _, diff in pairs)
 
 
 Matching = True
